2ca89f8f550218e81312907c347975f97b53168f
nickcam <ncameron@outlook.com.au>
2017-01-20 16:17:47 +1100

    Revert "Renamed folders and project to something better."
    This reverts commit aba4bc839fc8f5781ace2c75c58590b747acb49a.

---
+++ Ang2-Esri-NetCore/src/Ang2-Esri-NetCore/app/app.component.ts
@@ -0,0 +1,27 @@
+﻿
+import './rxjs-operators';
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-component',
+    template: `
+                <esri-map-view></esri-map-view>
+              `
+})
+
+export class AppComponent {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+
+
+    }
+}
+
+
+
+
+
