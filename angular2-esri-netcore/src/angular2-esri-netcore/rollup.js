eb66b874e75bc5da175256d3eadcb1ec56485f3a
Nicholas Cameron <ncameron@outlook.com.au>
2017-01-23 21:46:08 +1100

    rollup update.

--- angular2-esri-netcore/src/angular2-esri-netcore/rollup.js
+++ angular2-esri-netcore/src/angular2-esri-netcore/rollup.js
@@ -9,14 +9,35 @@ entry: 'wwwroot/app/main.aot.js',
 dest: 'wwwroot/dist/build.js', // output a single application bundle
 external: [
     //decalre all of the esri modules included as external references. Should have this array declared once and shared between load.ts and this file.
-    'esri/Map', 
-    'esri/views/MapView',
-    'esri/layers/GraphicsLayer',
-    'esri/geometry/Point',
-    'esri/symbols/SimpleMarkerSymbol',
+    'esri/Map',
     'esri/Graphic',
     'esri/Color',
-    'esri/core/accessorSupport/decorators'
+    'esri/Basemap',
+
+    'esri/core/watchUtils',
+    'esri/core/accessorSupport/decorators',
+    'esri/core/Accessor',
+    'esri/core/Evented',
+
+    'esri/views/MapView',
+    'esri/views/SceneView',
+
+    'esri/layers/GraphicsLayer',
+
+    'esri/geometry/Point',
+    'esri/geometry/ScreenPoint',
+    'esri/geometry/Polygon',
+    'esri/geometry/Polyline',
+    'esri/geometry/Multipoint',
+    'esri/geometry/Circle',
+    'esri/geometry/geometryEngine',
+
+    'esri/symbols/SimpleMarkerSymbol',
+    'esri/symbols/SimpleFillSymbol',
+    'esri/symbols/SimpleLineSymbol',
+
+    'esri/widgets/Compass',
+    'esri/widgets/Zoom'
 ],
 sourceMap: false,
 format: 'amd',
